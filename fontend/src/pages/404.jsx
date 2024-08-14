e8fad77ea7fdda0328ba743593cdd82682e5bb39
tyaG10 <titmambyves6@gmail.com>
2024-08-14 18:28:39 +0200

    index.css hero bg fixed

--- fontend/src/pages/404.jsx
+++ fontend/src/pages/404.jsx
@@ -1,9 +1,14 @@
 import {Link} from "react-router-dom";
 
+
+
 const NotFoundPage = () => {
   return (
     <div className="min-h-screen bg-cover bg-center flex flex-col justify-center items-center text-white" style={{backgroundImage: `url('/404.png')`}}>
+        
         <header className="absolute top-0 left-0 p-4 bg-black w-full">
+          
+          
           <Link>
             <img src="/netflix-logo.png" alt="Netflix" className="h-8" />
           </Link>
